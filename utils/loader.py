5640b77f1e3ff7cc24ce2727d969238785d6659c
Divya_1 <Divyabansal20>
2026-01-12 23:59:10 +0530

    Added data loader module with pandas support

---
+++ utils/loader.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import os
+
+def load_dataset(filepath):
+    try:
+        extension= os.path.splitext(filepath)[1].lower()
+        if extension=='csv':
+            df= pd.read_csv(filepath)
+        elif extension in ['xls','xlsx']:
+            df= pd.read_excel(filepath)
+        else:
+            return None, "Unsuppoted file format. Must be either excel or csv"
+        return df, None
+
+    except Exception as e:
+        return None, str(e)
+
