92f7524a5030ff62c73bb43203c7d6c93f519bf2
Divya_1 <Divyabansal20>
2026-01-13 12:39:25 +0530

    Created basic UI and connected engine to app

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from engine.auditor import completeness_check
+from engine.scorer import calculate_trust_score
+from utils.loader import load_dataset
+
+st.title("Data Trust Scoring System")
+st.write("Upload a dataset to evaluate its analytical readiness")
+uploaded_file= st.file_uploader("Choose a CSV or Excel file",type=['excel','xlsx'])
+
+if uploaded_file is not None:
+    df,error= load_dataset(uploaded_file)
+
+    if error:
+        st.error(f"Error loading the file: {error}")
+    else:
+        st.success("File loaded successfully!")
+    
+    results= completeness_check(df)
+    trustScore= calculate_trust_score(results)
+    st.metric(label="Overall Trust Score",value=f"{trustScore}%")
